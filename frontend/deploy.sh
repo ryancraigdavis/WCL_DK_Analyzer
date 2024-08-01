dd506c0b14b133ef2030bfd4dae6d288b1c83967
Ryan Davis <ryancraigdavis@github.com>
2024-07-31 22:34:17 -0700

    #17 Added new S3 references and lambda

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -7,8 +7,8 @@ cwd="$(pwd)"
 yarn build
 cd dist
 
-aws s3 cp . s3://dk-analyze-frontend/ --recursive
-aws cloudfront create-invalidation --distribution-id E1PIFMLD39KZYU --paths '/*'
+aws s3 cp . s3://wcl-analyzer-frontend/ --recursive
+aws cloudfront create-invalidation --distribution-id E2QQGL1YVUW0M3 --paths '/*'
 
 cd -
 rm -rf dist
